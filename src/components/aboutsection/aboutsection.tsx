7a5ce8b98cf2dad9d1d59d0240b4e041ecf38524
Cadu Falcão <kddu422@gmail.com>
2025-10-16 19:40:38 -0300

    AbouSection finalizada

--- src/components/aboutsection/aboutsection.tsx
+++ src/components/aboutsection/aboutsection.tsx
@@ -1,7 +1,10 @@
+import { Check } from "lucide-react";
+import { Button } from "../ui/button";
+
 export default function AboutSection() {
   return (
-    <div className="w-full py-16 [background:linear-gradient(71deg,_#520E1C_22.72%,_#7A1520_99.76%)]">
-      <div className="mx-auto max-w-[1280px] px-8">
+    <div className="w-full py-16 [background:linear-gradient(50deg,_#520E1C_22.72%,_#7A1520_99.76%)]">
+      <div className="mx-auto flex max-w-[1280px] items-center justify-around px-8">
         <video
           src="/Video1.mp4"
           playsInline
@@ -9,6 +12,30 @@ export default function AboutSection() {
           className="aspect-auto h-[645px] w-[360px] rounded-4xl"
           poster="/poster.jpg"
         ></video>
+        <div className="flex flex-col items-start gap-8">
+          <h3 className="mb-4 text-4xl text-white/90">Sobre Cecília Melo</h3>
+          <p className="w-[600px] text-white/90">
+            Apaixonada por transformar vidas pela nutrição, Cecília ajuda há 5
+            anos pessoas a atingirem seus objetivos com equilíbrio e prazer. Sua
+            abordagem personalizada combina nutrição clínica e esportiva,
+            baseada em ciência, respeitando a rotina de cada um e promovendo
+            resultados leves para saúde, disposição e bem-estar.
+          </p>
+          <ul className="space-y-2 text-white/90">
+            <li className="flex gap-2">
+              <Check /> Resultados sem Restrições
+            </li>
+            <li className="flex gap-2">
+              <Check /> Ciência e Estratégia
+            </li>
+            <li className="flex gap-2">
+              <Check /> Acompanhamento de Perto
+            </li>
+          </ul>
+          <Button variant="secondary" className="mt-4">
+            marcar consulta
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "bg-primary min-w-[276px] text-brand-secondary hover:bg-red-200 uppercase bg-[#FF6B6B] duration-150",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
@@ -34,7 +34,7 @@ const buttonVariants = cva(
       variant: "default",
       size: "default",
     },
-  }
+  },
 );
 
 function Button({
